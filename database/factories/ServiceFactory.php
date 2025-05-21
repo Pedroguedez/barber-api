d44872a4202f7ff0e59181b4c216b97e26b3f5e7
Pedroguedez <pedrogudes158@icloud.com>
2025-05-21 13:37:48 -0300

    teste Appointment

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class ServiceFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'price' => fake()->randomFloat(2, 10, 100),
+            'duration' => fake()->numberBetween(15, 120),
+            'active' => true,
+        ];
+    }
+
+}
--- tests/Feature/AppointmentTest.php
+++ tests/Feature/AppointmentTest.php
@@ -2,46 +2,47 @@
 
 namespace Tests\Feature;
 
+use App\Models\Service;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 
-
-class AgendamentoControllerTest extends TestCase
+class AppointmentTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic feature test example.
-     */
+
     public function test_store_agendamento_retorna_status_201(): void
     {
         // Criar um usuário de teste
         $cliente = User::factory()->create([
-            'nivel' => 'Administrador',
+            'level' => 'Admin',
             'telefone' => '11999999999',
-            'ativo' => true,
+            'active' => true,
         ]);
         $barbeiro = User::factory()->create([
-            'nivel' => 'Barbeiro',
+            'level' => 'Barber',
             'telefone' => '11889999999',
-            'ativo' => true,
+            'active' => true,
         ]);
+
+        $servico = Service::factory()->create();
         // Simular o login do usuário
         Sanctum::actingAs($cliente);
 
         // Fazer a requisição autenticada
         $response = $this->postJson('/api/agendamentos', [
-            'cliente_id' => $cliente->id,
-            'barbeiro_id' => $barbeiro->id,
+            'client_id' => $cliente->id,
+            'barber_id' => $barbeiro->id,
+            'services_id' => $servico->id,
             'data' => '2025-06-01',
-            'horario' => '14:00',
-            'status' => 'pendente',
-            'observacao' => 'Corte e barba'
+            'time' => '14:00',
+            'status' => 'pending',
+            'notes' => 'Corte e barba'
         ]);
 
         $response->assertStatus(201)
-                ->assertJsonStructure(['message', 'data']);
+                ->assertJsonStructure(['data']);
         }
 }
